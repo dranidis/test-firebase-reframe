292dd08087115a57bcaeacde92ecfc17a1e9e237
D Dranidis <dranidis@gmail.com>
2022-03-07 16:12:13 +0200

    Movs subraw to file

--- src/test_firebase/firebase/fb_reframe.cljs
+++ src/test_firebase/firebase/fb_reframe.cljs
@@ -1,8 +1,8 @@
 (ns test-firebase.firebase.fb-reframe
   (:require [re-frame.core :as re-frame]
             [day8.re-frame.tracing :refer-macros [fn-traced]]
-            [test-firebase.firebase.firebase-database :refer [set-value! default-set-success-callback default-set-error-callback]]
-            [test-firebase.firebase.firebase-auth :refer [get-current-user-uid]]))
+            [test-firebase.firebase.firebase-database :refer [set-value! default-set-success-callback default-set-error-callback on-value]]
+            [reagent.ratom :as ratom]))
 
 ;;  Effect for setting a value in firebase. Optional :success and :error keys for handlers
 (re-frame/reg-fx
@@ -10,7 +10,44 @@
  (fn-traced [{:keys [path data success error]}]
             (let [success-callback (if success success default-set-success-callback)
                   error-callback (if error error default-set-error-callback)]
-              (set-value! (concat ["users" (get-current-user-uid)] path)
+              (set-value! path
                           data
                           success-callback
                           error-callback))))
+
+(re-frame/reg-sub-raw
+ ::on-value
+ (fn [app-db [_ path]]
+   (on-value path
+             #(re-frame/dispatch [::write-to-temp (concat [:temp] path) %]))
+   (ratom/make-reaction
+    (fn [] (get-in @app-db (concat [:temp] path)))
+    :on-dispose #(re-frame/dispatch [::cleanup-temp (concat [:temp] path)]))))
+
+;; temp storage for fire-base reads
+(re-frame/reg-event-db
+ ::write-to-temp
+ (fn-traced [db [_ path data]]
+            (assoc-in db path data)))
+
+
+;; https://stackoverflow.com/questions/14488150/how-to-write-a-dissoc-in-command-for-clojure
+(defn dissoc-in
+  "Dissociates an entry from a nested associative structure returning a new
+  nested structure. keys is a sequence of keys. Any empty maps that result
+  will not be present in the new structure."
+  [m [k & ks]]
+  (if ks
+    (if-let [nextmap (get m k)]
+      (let [newmap (dissoc-in nextmap ks)]
+        (if (seq newmap)
+          (assoc m k newmap)
+          (dissoc m k)))
+      m)
+    (dissoc m k)))
+
+;; clean temp storage
+(re-frame/reg-event-db
+ ::cleanup-temp
+ (fn-traced [db [_ path]]
+            (dissoc-in db path)))
--- src/test_firebase/subs.cljs
+++ src/test_firebase/subs.cljs
@@ -1,31 +1,20 @@
 (ns test-firebase.subs
   (:require
    [re-frame.core :as re-frame]
-   [test-firebase.events :as events]
-   [test-firebase.firebase.firebase-database :refer [on-value]]
    [test-firebase.firebase.firebase-auth :refer [get-current-user-uid]]
-   [reagent.ratom :as ratom]))
-
-(re-frame/reg-sub-raw
- ::on-value
- (fn [app-db [_ path]]
-   (on-value (concat ["users" (get-current-user-uid)] path)
-             #(re-frame/dispatch [::events/write-to-temp (concat [:temp] path) %]))
-   (ratom/make-reaction
-    (fn [] (get-in @app-db (concat [:temp] path)))
-    :on-dispose #(re-frame/dispatch [::events/cleanup-temp (concat [:temp] path)]))))
+   [test-firebase.firebase.fb-reframe :as fb-reframe]))
 
 (re-frame/reg-sub
  ::available
  (fn [[_ id]]
-   (re-frame/subscribe [::on-value ["games" id "available"]]))
+   (re-frame/subscribe [::fb-reframe/on-value ["users" (get-current-user-uid) "games" id "available"]]))
  (fn [value]
    value))
 
 (re-frame/reg-sub
  ::group-with
  (fn [[_ id]]
-   (re-frame/subscribe [::on-value ["games" id "group-with"]]))
+   (re-frame/subscribe [::fb-reframe/on-value ["users" (get-current-user-uid) "games" id "group-with"]]))
  (fn [value]
    value))
 
