f7c6041bbf95d2314a4950451a6ce2785bd4e39d
Dimitris Dranidis <dranidis@gmail.com>
2022-03-08 16:07:02 +0200

    Change to another project

--- src/test_firebase/firebase/firebase_app.cljs
+++ src/test_firebase/firebase/firebase_app.cljs
@@ -3,10 +3,11 @@
 
 (defn init-app []
   (fb/initializeApp
-   #js {:apiKey "AIzaSyCLH4BlNSOfTrMlB_90Hsxg5cr3bn3p-7E",
-        :authDomain "help-me-pick-what-to-play.firebaseapp.com",
-        :databaseURL "https://help-me-pick-what-to-play-default-rtdb.europe-west1.firebasedatabase.app",
-        :projectId "help-me-pick-what-to-play",
-        :storageBucket "help-me-pick-what-to-play.appspot.com",
-        :messagingSenderId "780911312465",
-        :appId "1:780911312465:web:bbd9007195b3c630910270"}))
+   #js {:apiKey "AIzaSyCpMjjOO9t-TVrzJ-seEy-4MUnH9PWc-uc",
+        :authDomain "test-firebase-refr.firebaseapp.com",
+        :databaseURL "https://test-firebase-refr-default-rtdb.europe-west1.firebasedatabase.app",
+        :projectId "test-firebase-refr",
+        :storageBucket "test-firebase-refr.appspot.com",
+        :messagingSenderId "1052651152055",
+        :appId "1:1052651152055:web:4971b846529e25aa0ba332"}))
+
